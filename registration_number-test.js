a2f418e391756bb63a112c455aab1f84cf320453
axolalose <axolalose@gmail.com>
2019-08-27 15:25:41 +0200

    new changes

--- registration_number-test.js
+++ registration_number-test.js
@@ -1,31 +1,31 @@
 describe ("regNumber", function(){
     it("Should return registration number from Bellvile",function(){
         var numberReg = regNumber();
-        numberReg.regTown("CY 214 2455");
-        assert.deepEqual([],  numberReg.regTown()); 
+        numberReg.regList("CY 214 2455");
+        assert.deepEqual([],  numberReg.regList()); 
     })
 
     it("Should return not valid registration if the is two towns entered",function(){
         var numberReg = regNumber();
-        numberReg.regTown("CA 325 5254");
-        numberReg.regTown("CW 251 6356");
-        numberReg.regTown("CY 214 2455");
-        assert.deepEqual([],  numberReg.regTown()); 
+        numberReg.regList("CA 325 5254");
+        numberReg.regList("CW 251 6356");
+        numberReg.regList("CY 214 2455");
+        assert.deepEqual([],  numberReg.regList()); 
     })
     it("Should return registration number from Cape Town",function(){
         var numberReg = regNumber();
-        numberReg.regTown("CA 236 3255");
-        assert.deepEqual([],  numberReg.regTown()); 
+        numberReg.regList("CA 236 3255");
+        assert.deepEqual([],  numberReg.regList()); 
     })
     it("Should return registration number from Malmesbury",function(){
         var numberReg = regNumber();
-        numberReg.regTown("CW 566 6566");
-        assert.deepEqual([],  numberReg.regTown()); 
+        numberReg.regList("CW 566 6566");
+        assert.deepEqual([],  numberReg.regList()); 
     })
     it("Should return empty object if the is no registration and town entered",function(){
         var numberReg = regNumber();
-        numberReg.regTown(" ");
-        assert.deepEqual([],  numberReg.regTown()); 
+        numberReg.regList(" ");
+        assert.deepEqual([],  numberReg.regList()); 
     })
 
     it("Should return false when entered not expected towns e.g GF", function(){
